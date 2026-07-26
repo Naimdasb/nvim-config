b38c0a3405b91f2a95e1e42b025d0bda2e52dd30
douglas.valente <douglas.valente@ice.com>
2026-07-26 17:07:50 -0300

    --amend

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,22 +1,18 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		branch = 'master',
 		build = ":TSUpdate",
+		lazy = false,
+		branch = "main",
 		config = function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = { "lua", "javascript", "typescript", "tsx" },
-				highlight = {
-					enable = true,
-				},
-				incremental_selection = {
-					enable = true,
-					keymaps = {
-						init_selection = "<c-space>",
-						node_incremental = "<c-space>",
-						node_decremental = "<M-space>",
-					},
-				},
+			require("nvim-treesitter.config").setup({
+				ensure_installed = { "lua", "javascript", "typescript", "tsx", "jsx" },
+			})
+
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function()
+					pcall(vim.treesitter.start)
+				end,
 			})
 		end,
 	},
